8cf5a5ce1c0251567f45aa8e1d60efe97c565fd4
Vansa Pha <vansajm@vansas-imac.local>
2018-04-23 15:29:50 +0700

    Initial Commit

---
+++ MVVMCBeta2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVVMCBeta2
+//
+//  Created by Vansa Pha on 4/23/18.
+//  Copyright © 2018 Vansa Pha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
